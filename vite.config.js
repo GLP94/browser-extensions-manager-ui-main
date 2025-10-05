347aa5aa612562b2316497c823315b4434f77a15
Giovanni Luca Pinelli <giovannilucapinelli94@gmail.com>
2025-10-05 16:55:11 +0200

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteGhPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), viteGhPages()],
+  plugins: [react()],
   base: '/browser-extensions-manager-ui-main/',
 });
